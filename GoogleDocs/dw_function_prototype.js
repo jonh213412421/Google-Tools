218dfe23cf2781d569aaec6db74b0def1c16a05e
João Alves <138532454+jonh213412421@users.noreply.github.com>
2024-12-11 21:17:54 -0300

    Update dw_function_prototype.js

--- GoogleDocs/dw_function_prototype.js
+++ GoogleDocs/dw_function_prototype.js
@@ -1,9 +1,13 @@
   function dw() {
   const doc = DocumentApp.getActiveDocument();
   const body = doc.getBody();
+  const chunk = 20000;
   var size = PropertiesService.getScriptProperties();
   var inicio = PropertiesService.getScriptProperties();
   var fim = PropertiesService.getScriptProperties();
+  var num_parts = PropertiesService.getScriptProperties();
+  var test = PropertiesService.getScriptProperties();
+  test.setProperty('test', 1);
     const options = {
       method: "GET",
       headers: {
@@ -13,16 +17,31 @@
   const url = "https://www.python.org/ftp/python/3.12.8/Python-3.12.8.tar.xz".trim();
   const resposta = UrlFetchApp.fetch(url, options);
   const tamanho = resposta.getHeaders()['Content-Range'].slice(10);
-  body.appendParagraph(tamanho);
   size.setProperty('size', tamanho);
+  const num_partes = size.getProperty('size') / chunk;
+  num_parts.setProperty('num_parts', num_partes);
   inicio.setProperty('inicio', 0);
   fim.setProperty('fim', 20000);
   Logger.log(size.getProperty('size'));
   Logger.log(size.getProperty('inicio'));
   Logger.log(size.getProperty('fim'));
-  const headers = { 'Range': 'bytes=' + inicio + '-' + fim }; 
+  const headers = { 'Range': 'bytes=' + inicio + '-' + fim }; // Baixar do byte 0 até 2MB (primeiros 3MB)
   const options2 = { 'headers': headers };
   resposta2 = UrlFetchApp.fetch(url, options2);
   const blob = resposta2.getBlob().getBytes();
   Logger.log(blob.length);
+  Logger.log(Math.ceil(num_parts.getProperty('num_parts')));
+  let temp = test.getProperty('test');
+  var i = 0;
+  while (i < num_parts.getProperty('num_parts')) {
+    i += 1;
+    temp += i;
+    test.setProperty('test', temp);
+    Logger.log(test.getProperty('test'));
+  }
+  }
+
+  function test() {
+    var num_parts = PropertiesService.getScriptProperties();
+    Logger.log(Math.ceil(num_parts.getProperty('num_parts')));
   }
